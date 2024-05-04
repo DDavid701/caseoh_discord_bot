54877ee4a6f8ccaf655e24405875bdc7a7b96e7b
daviddussa <daviddussa0@gmail.com>
2024-05-04 14:42:16 +0200

    adding

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import random
 import discord
 import colorama
+from dotenv import load_dotenv
+import os
+colorama.init(autoreset=True)
+load_dotenv("options.ENV")
+TOKEN_FILE=os.getenv("TokenFile")
+STATS_FILE=os.getenv("StatsFile")
+CONFIG_FOLDER=os.getenv("ConfigFolder")
+FROM=os.getenv("FROM")
+TO=os.getenv("TO")
+TRIGGER=os.getenv("TRIGGER")
+SAVE_CHANNEL=os.getenv("SAVE_CHANNEL")
+load_dotenv(f'{CONFIG_FOLDER}/stats.env')
+EMBED_TITLE=os.getenv("EMBED_TITLE")
+COMMAND_DESCRIPTION=os.getenv("COMMAND_DESCRIPTION")
+DESCRIPTION=os.getenv("DESCRIPTION")
+ZERO_DESCRIPTION=os.getenv("ZERO_DESCRIPTION")
+load_dotenv(f'{CONFIG_FOLDER}/on_message.env')
+ONMESSAGE_TITLE=os.getenv("ONMESSAGE_TITLE")
+ONMESSAGE_DESCRIPTION=os.getenv("ONMESSAGE_DESCRIPTION")
+PREFIX=colorama.Fore.CYAN+"LOG "+colorama.Fore.LIGHTBLACK_EX+"× "+colorama.Fore.LIGHTWHITE_EX
 
-with open("token", "r") as file:
+print(PREFIX+"Getting Token....")
+with open(TOKEN_FILE, "r") as file:
     TOKEN=file.read()
+    print(PREFIX+f"The Token is '{TOKEN_FILE}'")
 
 def LUCK():
-    gen=random.randint(68,69)
-    print(gen)
-    if gen==69:
-        with open("stats", "r") as file:
+    gen=random.randint(int(FROM),int(TO))
+    if str(gen)==str(TRIGGER):
+        with open(STATS_FILE, "r") as file:
             LATEST_STATS=file.read()
-        LATEST_STATS=int(LATEST_STATS)
-        LATEST_STATS=LATEST_STATS+1
-        with open("stats", "w") as file:
+        LATEST_STATS=int(LATEST_STATS)+1
+        with open(STATS_FILE, "w") as file:
             file.write(str(LATEST_STATS))
         return 0
     else:
@@ -23,40 +43,42 @@ bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    print(f"Bot is running [{bot.user}]")
+    print(PREFIX+f"Bot is running [{bot.user}]")
 
 @bot.event
 async def on_message(msg):
-    CHANNEL=msg.channel
-    print(CHANNEL)
+    CHANNEL=str(msg.channel)
     if msg.author.bot:
         pass
     else:
-        if str(CHANNEL) == "no-eating":
+        if str(CHANNEL)==SAVE_CHANNEL:
             return
         else:
             CHANCE=LUCK()
             if CHANCE==1:
                 pass
             else:
-                embed = discord.Embed(title="MESSAGE EATEN :hamburger: :french_fries: :pizza:",
-                                      description="Caseoh ate your message! :hamburger: :french_fries: :pizza:",
+                embed = discord.Embed(title=ONMESSAGE_TITLE,
+                                      description=ONMESSAGE_DESCRIPTION,
                                       color=0x992d22)
-                print(msg.channel)
-                if "Direct Message" in str(msg.channel):
+                if "Direct Message" in CHANNEL:
                     pass
-                elif "Direct Message" not in str(msg.channel):
+                elif "Direct Message" not in CHANNEL:
                     await msg.delete()
                     await msg.channel.send(embed=embed)
+                else:
+                    pass
 
-@bot.command(description="Track the amount of messages eaten by caseoh")
+@bot.command(description=COMMAND_DESCRIPTION)
 async def stats(msg):
-    with open("stats", "r") as file:
+    with open(STATS_FILE, "r") as file:
         STATS=file.read()
         if STATS=="0":
-            embed = discord.Embed(title="CaseOh Stats", description=f"Case hasn't eaten any messages yet.", color=0x992d22)
+            embed = discord.Embed(title=EMBED_TITLE, description=ZERO_DESCRIPTION, color=0x992d22)
         else:
-            embed = discord.Embed(title="CaseOh Stats", description=f"Case already ate {STATS} messages.", color=0x992d22)
+            if "%STATS%" in DESCRIPTION:
+                DESCRIPTION_NEW=DESCRIPTION.replace("%STATS%", f"{STATS}")
+            embed = discord.Embed(title=EMBED_TITLE, description=DESCRIPTION_NEW, color=0x992d22)
     await msg.respond(embed=embed)
 
 bot.run(TOKEN)
---
+++ setup/install_requirements.sh
@@ -0,0 +1,6 @@
+echo "Installing Requirements from requirements.txt...."
+pip3 install requirements.txt
+pip install requirements.txt
+pip3 install requirements.txt --break-system-packages
+pip install requirements.txt --break-system-packages
+echo "Ending Installer...."
